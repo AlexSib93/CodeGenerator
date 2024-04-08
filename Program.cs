c3d3b0bc462c87f7f00fa1a2a650c63edab30fa8
Александр Сибиряков <AlexSib93@gmail.com>
2024-04-08 14:58:23 +0500

    Версия для проб

--- Program.cs
+++ Program.cs
@@ -1,7 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using CodeGenerator;
-string project = Environment.GetCommandLineArgs()[1];
+string project = Environment.GetCommandLineArgs().Count() > 1 
+    ? Environment.GetCommandLineArgs()[1]
+    : "DefaultProject";
 
-string fileType = Environment.GetCommandLineArgs()[2];
+string fileType = Environment.GetCommandLineArgs().Count() > 2
+    ? Environment.GetCommandLineArgs()[2]
+    : "";
 
 Generator.GenCode(project, fileType);
