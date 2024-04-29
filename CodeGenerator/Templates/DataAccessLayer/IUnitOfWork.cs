8fb03eeb8d9a9b82b065f0639523e28fa993752e
Александр Сибиряков <AlexSib93@gmail.com>
2024-04-29 15:19:13 +0500

    Mock репозиторий в шаблоне DataAccessLayer

---
+++ CodeGenerator/Templates/DataAccessLayer/IUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using DataAccessLayer.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IRepository<TestModel1> RepTestModel1 { get;}
+
+    }
+}
